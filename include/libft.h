fac3e8a8310d821e75745e6c02e303e865fba837
NinoCLS <ninoclasseau01@gmail.com>
2023-11-07 19:36:24 +0100

    add libft.h

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: nino <nino@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 14:49:19 by nino              #+#    #+#             */
-/*   Updated: 2023/11/07 19:18:02 by nino             ###   ########.fr       */
+/*   Updated: 2023/11/07 19:36:04 by nino             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ void    *ft_memset(void *str, int c, size_t n);
 void    ft_bzero(void *s, size_t n);
 int ft_strlen(char *str);
 size_t  ft_strlcat(char *dest, const char *src, size_t destsize);
+
 
 
 
